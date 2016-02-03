53f02e8efb030196673d07eb67b848c15df258f0
Ryan Kliemann <Ryan_blake@icloud.com>
2016-02-02 20:36:33 -0600

    get Jobs to sample clients

--- app/models/client.rb
+++ app/models/client.rb
@@ -2,11 +2,21 @@
 #
 # Table name: clients
 #
-#  id         :integer          not null, primary key
-#  name       :string
-#  client_id  :integer
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id                     :integer          not null, primary key
+#  name                   :string
+#  client_id              :integer
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#  email                  :string           default(""), not null
+#  encrypted_password     :string           default(""), not null
+#  reset_password_token   :string
+#  reset_password_sent_at :datetime
+#  remember_created_at    :datetime
+#  sign_in_count          :integer          default(0), not null
+#  current_sign_in_at     :datetime
+#  last_sign_in_at        :datetime
+#  current_sign_in_ip     :string
+#  last_sign_in_ip        :string
 #
 
 class Client < ActiveRecord::Base
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,55 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+#Creates Users
+5.times do
+  client = Client.new(
+  # #3
+  email:   Faker::Internet.email,
+  name:     Faker::Name.name,
+  password: "testtest"
+  )
+  # client.skip_confirmation!
+  client.save!
+end
+clients = Client.all
+
+#Creates Users
+5.times do
+  laborer = Laborer.new(
+  # #3
+  email:   Faker::Internet.email,
+  name:     Faker::Name.name,
+  password: "testtest"
+  )
+  # laborer.skip_confirmation!
+  laborer.save!
+end
+laborers = Laborer.all
+
+
+# Creates i
+20.times do
+  Job.create!(
+  title:       Faker::Hipster.word,
+  description:    Faker::Hipster.paragraph,
+  job_id:   clients.sample,
+  # created_at: "2015-12-10 20:24:01"
+  )
+end
+
+# Create an admin user
+# admin = User.new(
+#   name:     'Admin User',
+#   email:    'admin@example.com',
+#   password: 'testtest',
+#   role:     'admin'
+# )
+# admin.skip_confirmation!
+# admin.save!
+
+jobs = Job.all
+
+
+puts "Seed finished"
+puts "#{Client.count}  clients created"
+puts "#{Laborers.count}  laborers created"
+puts "#{Job.count}  jobs created"
