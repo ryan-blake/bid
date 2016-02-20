a2fcda73bb8502f3b4d845e8d1694473f5effbd2
Ryan Kliemann <Ryan_blake@icloud.com>
2016-02-20 13:21:13 -0600

    got ratings

--- app/models/laborer.rb
+++ app/models/laborer.rb
@@ -29,6 +29,7 @@ class Laborer < ActiveRecord::Base
   has_many :clients, through: :jobs
   enum role: [:standard, :premium, :admin]
   has_many :submits, :as => :submittable
+  has_many :reviews
 
 
   def init
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,17 +1,3 @@
-# == Schema Information
-#
-# Table name: reviews
-#
-#  id         :integer          not null, primary key
-#  title      :string
-#  author     :string
-#  content    :string
-#  rating     :integer
-#  laborer_id :integer
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#
-
 require 'rails_helper'
 
 RSpec.describe Review, type: :model do
