3a148c5ffe3035025e2683696526c684db9be055
Ryan Kliemann <Ryan_blake@icloud.com>
2016-03-07 15:32:59 -0600

    baduser/pass-sendgrid

--- config/environment.rb
+++ config/environment.rb
@@ -9,6 +9,6 @@ ActionMailer::Base.smtp_settings = {
   :authentication => :plain,
   :user_name      => ENV['SENDGRID_USERNAME'],
   :password       => ENV['SENDGRID_PASSWORD'],
-  :domain         => 'heroku.com',
+  :domain         => 'bidscape.heroku.com',
   :enable_starttls_auto => true
 }
