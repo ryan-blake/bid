dd22a823482ec55ed9241ccc455cf07179754d26
Ryan Kliemann <Ryan_blake@icloud.com>
2016-03-10 10:28:49 -0600

    test pro

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -12,7 +12,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'bids@example.com'
+  config.mailer_sender = ENV["SENDGRID_USERNAME"]
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
@@ -262,4 +262,7 @@ Devise.setup do |config|
   # When using OmniAuth, Devise cannot automatically set OmniAuth path,
   # so you need to do it manually. For the clients scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/clients/auth'
+
+  config.mailer_sender = ENV["SENDGRID_USERNAME"]
+
 end
--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -6,7 +6,7 @@ if Rails.env.development? || Rails.env.production?
      authentication: :plain,
      user_name:      ENV['SENDGRID_USERNAME'],
      password:       ENV['SENDGRID_PASSWORD'],
-     domain:         'bidscape.heroku.com',
+     domain:         'heroku.com',
      enable_starttls_auto: true
    }
  end
