ed393e67ba73e5f51e7384439ffd7b827f87bbc5
Ryan Kliemann <Ryan_blake@icloud.com>
2016-03-10 10:16:09 -0600

    test-pro.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,5 +78,17 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.default_url_options = { host: 'localhost' }
 
+  config.action_mailer.default_url_options = { :host =&gt; 'bidscape.herokuapp.com'}
 
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+  address: "smtp.sendgrid.net",
+  port: 25,
+  domain: "heroku.com",
+  authentication: "plain",
+  enable_starttls_auto: true,
+  user_name: ENV["SENDGRID_USERNAME"],
+  password: ENV["SENDGRID_PASSWORD"]
+  }
 end
