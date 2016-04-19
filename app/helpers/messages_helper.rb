ac19b84e0592a361134aa5b1d38ddda9095176a6
Ryan Kliemann <Ryan_blake@icloud.com>
2016-04-19 10:47:56 -0500

    updating chosen_recipient

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -3,7 +3,7 @@ module MessagesHelper
   def recipients_options(chosen_recipient = nil)
     s = ''
     selectors.each do |user|
-      s << "<option value='#{user.id}'>#{user.name}</option>"
+      s << "<option value='#{user.id}' #{ 'selected' if chosen_recipient == user }>#{user.name}</option>"
     end
     s.html_safe
   end
