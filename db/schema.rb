b6acb7658ebda258f1b6b56b9cb7bf1f32bd9b5b
Ryan Kliemann <Ryan_blake@icloud.com>
2016-05-06 01:00:54 -0500

    update migration for images

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160505031641) do
+ActiveRecord::Schema.define(version: 20160506060024) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
