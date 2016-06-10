58629c75e0f869c7dd19f8c76e8107fcd77cf8f5
Ryan Kliemann <Ryan_blake@icloud.com>
2016-06-10 00:35:45 -0500

    regionID

--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -5,7 +5,7 @@ aws = {
   access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
   secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
   bucket: ENV.fetch('S3_BUCKET_NAME'),
-  region: ENV.fetch('region_id')
+  region: ENV.fetch('REGION_ID')
 }
 Refile.cache = Refile::S3.new(prefix: "cache", **aws)
 Refile.store = Refile::S3.new(prefix: "store", **aws)
